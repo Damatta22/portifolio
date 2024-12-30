28dc2148eaf5a102affe7a8de240cdb56763aa34
Damatta22 <flaviodamataflv@gmail.com>
2024-12-30 15:30:18 -0300

    add flag

--- src/components/information/information.jsx
+++ src/components/information/information.jsx
@@ -1,12 +1,16 @@
 import "./information.css";
+import BRflag from "../../assets/BRAflag.png"
+import USflag from "../../assets/USAflag.jpg"
 
 export function Info() {
   return (
     <div className="infos">
       <div className="languages-info">
         <h1>Languages</h1>
-        <span> 🇺🇸 EN- Intermediary</span>
-        <span>🇧🇷 PT-BR- Native Speaker</span>
+       
+        <span> <img src={USflag} width={30} height={30}alt="flagimg"></img> 🇺🇸 EN- Intermediary</span>
+       
+        <span> <img src={BRflag} width={30} height={30}alt="flagimg"></img>🇧🇷 PT-BR- Native Speaker</span>
       </div>
 
       <div className="educational-info">
